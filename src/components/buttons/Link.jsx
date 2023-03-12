1543dfbbc7724d7b7bd3c7e7f1a9be6c865dbfc8
Gark Godwin Duque <garkgodwinduque@gmail.com>
2023-03-12 05:35:34 -0600

    new: added folders and button components

---
+++ src/components/buttons/Link.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Link = () => {
+  return <a>Link</a>;
+};
+
+export default Link;
---
+++ src/components/buttons/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./Button";
+import Link from "./Link";
+import NavItem from "./NavItem";
+
+module.exports = {
+  Button,
+  Link,
+  NavItem,
+};

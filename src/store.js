0f694490993d5a24b340d05553122de1e40e8a01
Gark Godwin Duque <garkgodwinduque@gmail.com>
2023-03-25 05:08:27 -0400

    fix: removed 2 unused features

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./features/counterSlice";
-import tableSlice from "./features/tableSlice";
 import usersSlice from "./features/usersSlice";
 import todosSlice from "./features/todosSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
-    tables: tableSlice,
     users: usersSlice,
     todos: todosSlice,
   },

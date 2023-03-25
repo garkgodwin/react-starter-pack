a398ca3838d64d6b3d28c2b321659b6a9de95cd0
Gark Godwin Duque <garkgodwinduque@gmail.com>
2023-03-25 05:03:56 -0400

    feat: added reducers: remove, select, unSelect

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./features/counterSlice";
 import tableSlice from "./features/tableSlice";
+import usersSlice from "./features/usersSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
     tables: tableSlice,
+    users: usersSlice,
   },
 });

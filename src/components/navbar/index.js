b136acc54599238af3121c83f00bdd687449d6b6
Gark Godwin Duque <garkgodwinduque@gmail.com>
2023-03-25 06:39:41 -0400

    feat: added some design and logo in navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,11 +4,16 @@ import React from "react";
 import "./index.css";
 //? COMPONENTS
 import NavbarLink from "./NavbarLink";
+//? IMAGES
+import logo from "../../images/logo.svg";
 
 const Navbar = () => {
   return (
     <div id="Navbar">
-      <div className="navbar-brand">Brand</div>
+      <div className="navbar-brand">
+        <img src={logo} alt="Logo" />
+      </div>
+
       <nav className="nav-items">
         <NavbarLink text="Home" link="/" />
         <NavbarLink text="Login" link="/login" />

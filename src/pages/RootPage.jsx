40c0bd825256955fa6628e2186b5bb397816ef5d
Gark Godwin Duque <garkgodwinduque@gmail.com>
2023-03-25 07:25:50 -0400

    feat: added styles

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const RootPage = () => {
-  return <main>RootPage</main>;
+  return (
+    <main>
+      <section className="row row-wrap">
+        <div className="bc1 col col-center wpe-50 hpx-400">
+          <h1 className="c2 fs4">Welcome to my React Starter Pack</h1>
+        </div>
+        <div className="bc1 col wpe-50 hpx-400"></div>
+        <div className="bc1 col wpe-50 hpx-400"></div>
+        <div className="bc2 col col-center wpe-50 hpx-400">
+          <span className="c1 fs1">
+            Build your react application with this base package.
+          </span>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default RootPage;

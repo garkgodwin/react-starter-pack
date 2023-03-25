30187dc1ba419056e4affad9b4c31453ab39b1de
Brixter Porras <brixterporras@gmail.com>
2023-03-26 03:19:18 +0800

    refactor: user auth state to redux persist

--- src/index.js
+++ src/index.js
@@ -11,8 +11,23 @@ import "./fonts/Roboto/Roboto-Regular.ttf";
 //? STORE
 import { store } from "./store";
 
+// ? redux persist for autosaving state to localstorage
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+export const persistor = persistStore(store);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <PersistGate
+      persistor={persistor}
+      loading={
+        <div>
+          {/* TODO! add loader for persistor */}
+          Loading
+        </div>
+      }
+    >
+      <RouterProvider router={router} />
+    </PersistGate>
   </Provider>
 );

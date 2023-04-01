79c814305bc7f9f80e906f4f76ef02dd7f011d24
Gark Godwin Duque <garkgodwinduque@gmail.com>
2023-04-01 06:55:47 -0400

    ref: removed auth on mount temporarily

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-/*
-  This is the App component,
-  will contain the following:
-  1. Checks everytime on mount the token saved in local storage.
-  2. If authenticated, fetches all the data needed from API.
-  3. All data fetched in API must be saved in the "STORE".
-  4. Outlet to call all the children for this specific component
-*/
+/**
+ * @author Gark
+ * @returns {App} the main component of this project
+ * This is the App component,
+ * will contain the following:
+ * 1. Checks everytime on mount the token saved in local storage.
+ * 2. If authenticated, fetches all the data needed from API.
+ * 3. All data fetched in API must be saved in the "STORE".
+ * 4. Outlet to call all the children for this specific component
+ * TODO: add/update comments
+ */
 
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -16,24 +19,12 @@ import Sidebar from "./components/sidebar";
 
 import "./pages/index.css";
 
-import { authenticate } from "./features/authSlice";
-
 const App = () => {
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   // AUTHENTICATE
   // FETCH DATA
   // SAVE TO STORE
-
-  // authenticate on mount
-  useEffect(() => {
-    authenticateToken();
-  }, []);
-
-  // call "authenticate" from "auth reducers"
-  const authenticateToken = () => {
-    dispatch(authenticate());
-  };
   return (
     <div className={"App" + (auth.user ? " app-authed" : "")}>
       {auth.user ? <Sidebar /> : <Navbar />}
